beaee7b52fbb3174374b23bddb394d2262cf7fd4
Pablo Dafonte Iglesias <pablo.dafonte.iglesias@gmail.com>
2025-08-03 22:31:12 +0200

    consulta esquemas

--- postgres_citus_columnar/scripts/00_create_database.sql
+++ postgres_citus_columnar/scripts/00_create_database.sql
@@ -3,3 +3,9 @@ CREATE DATABASE personas;
 -- Se debe cambiar a la nueva base de datos creada, 
 -- para instalar la estensión en la base de datos
 CREATE EXTENSION citus_columnar;
+
+-- Consultar esquemas creados
+
+SELECT schema_name
+FROM information_schema.schemata
+ORDER BY schema_name;
